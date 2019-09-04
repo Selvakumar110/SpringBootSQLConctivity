8aa858d6c69de83b68332c2207d78dec51bec7e6
selva1521 <54443783+selva1521@users.noreply.github.com>
2019-09-04 10:33:12 +0530

    Update LibraryDao.java

--- dao/LibraryDao.java
+++ dao/LibraryDao.java
@@ -1,14 +1,12 @@
 package com.library.dao;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Component;
 
 import com.library.model.Library;
@@ -35,17 +33,7 @@ public class LibraryDao {
 
 	}
 
-	public List<Library> getall(Integer pageNo, Integer pageSize) {
-		Pageable paging = PageRequest.of(pageNo, pageSize);
-		
-		Page<Library> pagedResult = repository.findAll(paging);
-		
-		if (pagedResult.hasContent()) {
-			return pagedResult.getContent();
-		} else {
-			return new ArrayList<Library>();
-		}
-	}
+
 
 	public List<Library> getAll() {
 		logger.info("Hey I am Logger in Get ALL Section");
